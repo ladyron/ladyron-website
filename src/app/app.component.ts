41ba93f4d3b7d6540376157b3bc09910dc0fe6de
Roni Ruadap <roni.ruadap@gmail.com>
2024-07-03 00:01:50 -0400

    add profile component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { ProfileComponent } from "./profile/profile.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent, ProfileComponent]
 })
 export class AppComponent {
   title = 'Lady Ron | Portfolio Website';
